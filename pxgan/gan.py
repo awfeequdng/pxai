0cfbc842cb500ba12d353be922db9c8f1222d3fc
caipengxiang <291458254@qq.com>
2022-07-08 10:07:58 +0800

    save and load model

--- pxgan/gan.py
+++ pxgan/gan.py
@@ -82,44 +82,54 @@ test_input = torch.randn(16, 100, device=device)
 D_loss = []
 G_loss = []
 
-for epoch in range(20):
-    d_epoch_loss = 0
-    g_epoch_loss = 0
-    count = len(dataloader)
-    for step, (imgs, _) in enumerate(dataloader):
-        imgs = imgs.to(device)
-        size = imgs.size(0)
-        random_noise = torch.randn(size, 100, device=device)
+train = True
+
+max_epoch = 10
+
+if train:
+    for epoch in range(max_epoch + 1):
+        d_epoch_loss = 0
+        g_epoch_loss = 0
+        count = len(dataloader)
+        for step, (imgs, _) in enumerate(dataloader):
+            imgs = imgs.to(device)
+            size = imgs.size(0)
+            random_noise = torch.randn(size, 100, device=device)
 
 
-        d_optim.zero_grad()
-        real_output = dis(imgs)
-        d_real_loss = loss_fn(real_output, torch.ones_like(real_output))
-        d_real_loss.backward()
+            d_optim.zero_grad()
+            real_output = dis(imgs)
+            d_real_loss = loss_fn(real_output, torch.ones_like(real_output))
+            d_real_loss.backward()
 
-        gen_imgs = gen(random_noise)
-        fake_output = dis(gen_imgs.detach())
-        d_fake_loss = loss_fn(fake_output, torch.zeros_like(fake_output))
-        d_fake_loss.backward()
+            gen_imgs = gen(random_noise)
+            fake_output = dis(gen_imgs.detach())
+            d_fake_loss = loss_fn(fake_output, torch.zeros_like(fake_output))
+            d_fake_loss.backward()
 
-        d_loss = d_real_loss + d_fake_loss
-        d_optim.step()
+            d_loss = d_real_loss + d_fake_loss
+            d_optim.step()
 
-        g_optim.zero_grad()
-        fake_output = dis(gen_imgs)
-        g_loss = loss_fn(fake_output, torch.ones_like(fake_output))
-        g_loss.backward()
-        g_optim.step()
+            g_optim.zero_grad()
+            fake_output = dis(gen_imgs)
+            g_loss = loss_fn(fake_output, torch.ones_like(fake_output))
+            g_loss.backward()
+            g_optim.step()
+
+            with torch.no_grad():
+                d_epoch_loss += d_loss
+                g_epoch_loss += g_loss
 
         with torch.no_grad():
-            d_epoch_loss += d_loss
-            g_epoch_loss += g_loss
+            d_epoch_loss /= count
+            g_epoch_loss /= count
+            D_loss.append(d_epoch_loss)
+            G_loss.append(g_epoch_loss)
+            print(f'epoch: {epoch}')
+            # gen_img_plot(gen, epoch, test_input)
+            if epoch == max_epoch:
+                torch.save(gen, f"./model/{max_epoch}/gen_model")
+else:
+    gen.load_state_dict(f"./model/{max_epoch}/gen_model")
 
-    with torch.no_grad():
-        d_epoch_loss /= count
-        g_epoch_loss /= count
-        D_loss.append(d_epoch_loss)
-        G_loss.append(g_epoch_loss)
-        print(f'epoch: {epoch}')
-        gen_img_plot(gen, epoch, test_input)
-
+gen_img_plot(gen, max_epoch, test_input)
